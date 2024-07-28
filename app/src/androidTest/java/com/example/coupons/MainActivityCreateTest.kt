e95be2eeb683da8e5fcdb87292b21487b68c0b0a
Angel Angeles <angel.angeles.molina@gmail.com>
2024-07-27 19:44:48 -0600

    Expresso testing

---
+++ app/src/androidTest/java/com/example/coupons/MainActivityCreateTest.kt
@@ -0,0 +1,34 @@
+package com.example.coupons
+
+import android.graphics.ColorSpace.match
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.coupons.mainModule.view.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityCreateTest {
+
+    //forma de importar recursos reutilizables, con esto se tendra acceso al mainActivity
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun createCouponTest() {
+        val etCoupon = onView(withId(R.id.etCoupon))//acceder a la vista en tiempo real
+        etCoupon.check(matches(withText("")))//verifica que(view) coincida (con el texto(""))
+
+    }
+
+
+}
