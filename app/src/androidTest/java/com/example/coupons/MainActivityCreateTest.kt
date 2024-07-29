1e418602162e51eb2f1c4a815d2907f134c09225
Angel Angeles <angel.angeles.molina@gmail.com>
2024-07-28 18:56:21 -0600

    displayText, isDisplayed, perform, event click

--- app/src/androidTest/java/com/example/coupons/MainActivityCreateTest.kt
+++ app/src/androidTest/java/com/example/coupons/MainActivityCreateTest.kt
@@ -2,7 +2,10 @@ package com.example.coupons
 
 import android.graphics.ColorSpace.match
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -27,6 +30,16 @@ class MainActivityCreateTest {
     fun createCouponTest() {
         val etCoupon = onView(withId(R.id.etCoupon))//acceder a la vista en tiempo real
         etCoupon.check(matches(withText("")))//verifica que(view) coincida (con el texto(""))
+        etCoupon.perform(click())
+        etCoupon.perform(replaceText("WELCOME_01"))
+
+        val btnConsultar = onView(withId(R.id.btnConsult))
+        btnConsultar.perform(click())
+
+         val btnCreate= onView(withId(R.id.btnCreate))
+        btnCreate.check(matches(isDisplayed())) //verificar si el componente es visible.
+
+
 
     }
 
